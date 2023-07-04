cd735e0ae70408460a7305b4b6c287a25e72c15f
kiratopat <capacity1412@gmail.com>
2023-07-04 17:40:39 +0700

    Set Handler on AuthenticationException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +28,9 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['message' => 'Unauthenticated'], Response::HTTP_UNAUTHORIZED);
     }
 }
