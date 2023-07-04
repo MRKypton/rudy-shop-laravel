75c47963c9173be078181cfcdd52f2ada62d94d2
kiratopat <capacity1412@gmail.com>
2023-07-04 17:12:12 +0700

    create a base API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to Rudy shop API'], Response::HTTP_OK);
 });
